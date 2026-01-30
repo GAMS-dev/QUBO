00588e8b6eadba4042154ea61622f8e8e16101bf
vjha <vjha@gams.com>
2026-01-30 12:03:23 +0100

    fix relative imports

--- gamspy_qubo/src/gamspy_qubo/backend/__init__.py
+++ gamspy_qubo/src/gamspy_qubo/backend/__init__.py
@@ -1,4 +1,4 @@
-from .base import baseBackend
-from .dwave import DwaveBackend
+from gamspy_qubo.backend.base import baseBackend
+from gamspy_qubo.backend.dwave import DwaveBackend
 
 __all__ = ["baseBackend", "DwaveBackend"]
