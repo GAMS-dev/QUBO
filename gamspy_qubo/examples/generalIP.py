57d7d37e3029995c11795bb7809fedbff8c559cf
vjha <vjha@gams.com>
2026-02-05 15:46:49 +0100

    update example

--- gamspy_qubo/examples/generalIP.py
+++ gamspy_qubo/examples/generalIP.py
@@ -29,7 +29,7 @@ c2 = gp.Equation(m, name="c2")
 c3 = gp.Equation(m, name="c3")
 
 obj[...] = (
-    gp.Sum(i, cost[i] * x[i]) + gp.Sum(i, cost[i] * y[i]) - gp.Sum(i, newX[i]) == -z
+    gp.Sum(i, cost[i] * x[i]) + gp.Sum(i, cost[i] * y[i]) - gp.Sum(i, newX[i]) == z
 )
 
 c1[...] = gp.Sum(i, x[i]) <= 20
@@ -41,17 +41,12 @@ demo_model = gp.Model(
     name="demo_model",
     problem="MIP",
     equations=m.getEquations(),
-    sense=gp.Sense.MIN,
+    sense=gp.Sense.MAX,
     objective=z,
 )
 
-# q = Qubo(demo_model, penalty=10, backend="cplex")
 q = Qubo(demo_model, penalty=10, backend="dwave")
-
-# q.solve()
 q.solve(num_reads=1000)
-# NOTE: I tried with different order of penalties. It gives consistent result for 1e4,
-# anything less gives imprecise and un-accurate results.
 
 print(f"Original Objective Variable:\n{demo_model._objective_variable.records}")
 print(f"Variable x:\n{x.records}")
