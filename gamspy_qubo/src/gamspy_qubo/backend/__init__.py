374448acdfe0de83beba3d798913b64c634cc6d2
Janina Hasselbring <jhasselbring@gams.com>
2026-06-26 15:16:52 +0200

    add fxstars to backends

--- gamspy_qubo/src/gamspy_qubo/backend/__init__.py
+++ gamspy_qubo/src/gamspy_qubo/backend/__init__.py
@@ -1,6 +1,13 @@
 from gamspy_qubo.backend.base import baseBackend
 from gamspy_qubo.backend.dwave import DwaveBackend
+from gamspy_qubo.backend.fixstars_amplify import FixstarsBackend
 from gamspy_qubo.backend.kipu import KipuBackend
 from gamspy_qubo.backend.load_json import JsonBackend
 
-__all__ = ["baseBackend", "DwaveBackend", "KipuBackend", "JsonBackend"]
+__all__ = [
+    "baseBackend",
+    "DwaveBackend",
+    "KipuBackend",
+    "JsonBackend",
+    "FixstarsBackend",
+]
